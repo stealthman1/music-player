aac6a30520edda42bbb589fe0b8cb3e6b6202580
stealthman1 <timur.marufiy@gmail.com>
2022-08-05 17:46:21 +0300

    finishing coding progress bar

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ loadSong(songs[songIndex]);
 // Update Progress Bar
 function updateProgressBar(e) {
     if (isPlaying) {
-        const {duration, currentTime} = e.srcElement;
+        const { duration, currentTime } = e.srcElement;
         // Update progress bar width
         const progressPercent = (currentTime / duration) * 100;
         progress.style.width = `${progressPercent}%`;
@@ -113,12 +113,26 @@ function updateProgressBar(e) {
         let currentSeconds = Math.floor(currentTime % 60);
         if (currentSeconds < 10) {
             currentSeconds = `0${currentSeconds}`;
-            currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`
         }
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`
     }
+}
+
+// Set Progress Bar
+function setProgressBar(e) {
+    const width = this.clientWidth;
+    console.log('width', width);
+    const clickX = e.offsetX;
+    console.log('clickX', clickX);
+    const { duration } = music;
+    console.log(clickX / width);
+    console.log((clickX / width) * duration);
+    music.currentTime = (clickX / width) * duration;
 }
 
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
